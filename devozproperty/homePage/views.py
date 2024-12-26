22241927f54747ae8e17d1ee25ee301f84e09aee
Mahi Uddin <mahi.ewu@gmail.com>
2024-12-26 07:02:38 +0600

    bootstrap added

--- devozproperty/homePage/views.py
+++ devozproperty/homePage/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 def homePage(request):
-  return HttpResponse("This is the new file")
+  template = loader.get_template('home.html')
+  return HttpResponse(template.render())
